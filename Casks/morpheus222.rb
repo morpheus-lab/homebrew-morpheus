b7e23dd249657ab83b40c40ca6cd9fafe111b60d
Diego Jahn <jahndiego@mailbox.org>
2021-11-19 09:32:34 +0100

    Add Morpheus.app >=v1.2.1

---
+++ Casks/morpheus222.rb
@@ -0,0 +1,31 @@
+cask "morpheus222" do
+  version "2.2.2"
+  sha256 "968cadb06fbc1d5520d84d116d4c5bd94aaf881d3ed48bc7197b6b003a02a384"
+
+  url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/Morpheus-#{version}.dmg",
+      verified: "imc.zih.tu-dresden.de/morpheus/"
+  name "Morpheus"
+  desc "Modelling environment for multi-cellular systems biology"
+  homepage "https://morpheus.gitlab.io/"
+
+  livecheck do
+    url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/"
+    strategy :page_match
+    regex(/href=.*?Morpheus[._-](\d+(?:\.\d+)*)\.dmg/i)
+  end
+
+  app "Morpheus.app"
+
+  zap trash: [
+    "~/Library/Application Support/data/Morpheus",
+    "~/Library/Application Support/Morpheus",
+    "~/Library/Application Support/CrashReporter/morpheus_*.plist",
+    "~/Library/Caches/Morpheus",
+    "~/Library/Preferences/morpheus-gui.plist",
+    "~/Library/Preferences/io.gitlab.morpheus.morpheus.plist",
+    "~/Library/Preferences/de.tu-dresden.Morpheus.plist",
+    "~/Library/Preferences/org.morpheus.Morpheus.plist",
+    "~/Library/Saved Application State/de.tu-dresden.Morpheus.savedState",
+    "~/Library/Saved Application State/io.gitlab.morpheus.morpheus.savedState",
+  ]
+end
---
+++ Casks/morpheus2251.rb
@@ -0,0 +1,31 @@
+cask "morpheus2251" do
+  version "2.2.5.1"
+  sha256 "39823039b329e3edbcae263e0ecd3b3d30c6c0e0d2fc523946336b3d3ba8bdbf"
+
+  url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/Morpheus-#{version}.dmg",
+      verified: "imc.zih.tu-dresden.de/morpheus/"
+  name "Morpheus"
+  desc "Modelling environment for multi-cellular systems biology"
+  homepage "https://morpheus.gitlab.io/"
+
+  livecheck do
+    url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/"
+    strategy :page_match
+    regex(/href=.*?Morpheus[._-](\d+(?:\.\d+)*)\.dmg/i)
+  end
+
+  app "Morpheus.app"
+
+  zap trash: [
+    "~/Library/Application Support/data/Morpheus",
+    "~/Library/Application Support/Morpheus",
+    "~/Library/Application Support/CrashReporter/morpheus_*.plist",
+    "~/Library/Caches/Morpheus",
+    "~/Library/Preferences/morpheus-gui.plist",
+    "~/Library/Preferences/io.gitlab.morpheus.morpheus.plist",
+    "~/Library/Preferences/de.tu-dresden.Morpheus.plist",
+    "~/Library/Preferences/org.morpheus.Morpheus.plist",
+    "~/Library/Saved Application State/de.tu-dresden.Morpheus.savedState",
+    "~/Library/Saved Application State/io.gitlab.morpheus.morpheus.savedState",
+  ]
+end
