b6872ecbb927a8c76d47b9b5234261714a33ef8a
Diego <diego.jahn@tu-dresden.de>
2021-12-07 15:55:16 +0100

    Add morpheus-develop (new formula) (#6)

---
+++ Formula/morpheus-dev.rb
@@ -0,0 +1,107 @@
+class MorpheusDev < Formula
+  desc "Modelling environment for multi-cellular systems biology"
+  homepage "https://morpheus.gitlab.io/"
+  # url "https://gitlab.com/morpheus.lab/morpheus/-/archive/v2.2.6/morpheus-v2.2.6.tar.gz"
+  sha256 "ed39e034eb47972af730b1538a005d7703cf5817f39452fae3c9fdc0bf9efd23"
+  license "BSD-3-Clause"
+  head "https://gitlab.com/morpheus.lab/morpheus.git", branch: "release_2.2"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+(?:-[a-z]+\d*)?)$/i)
+  end
+
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build
+  depends_on "ffmpeg" # Runtime dependencies
+  depends_on "gnuplot"
+  depends_on "graphviz"
+  depends_on "libomp"
+  depends_on "libtiff"
+  depends_on "qt@5"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def install
+    args = std_cmake_args
+    args << "-DMORPHEUS_RELEASE_BUNDLE=ON" if OS.mac?
+
+    system "cmake", ".", *args
+    system "make", "install"
+
+    if OS.mac?
+      bin.write_exec_script "#{prefix}/Morpheus.app/Contents/MacOS/morpheus"
+
+      (bin/"morpheus-gui").write <<~EOS
+        #!/bin/bash
+        open #{prefix}/Morpheus.app
+      EOS
+      (bin/"morpheus-gui").chmod 0555
+    end
+  end
+
+  def post_install
+    if OS.mac?
+      # Set PATH environment variable including Homebrew prefix in macOS app bundle
+      inreplace "#{prefix}/Morpheus.app/Contents/Info.plist", "<key>CFBundleExecutable</key>",
+        <<~EOS.chomp
+          <key>LSEnvironment</key>
+          <dict>
+              <key>PATH</key>
+              <string>#{ENV["PATH"]}</string>
+          </dict>
+          <key>CFBundleExecutable</key>
+        EOS
+    end
+  end
+
+  def caveats
+    if OS.mac?
+      <<~EOS
+        To start the Morpheus GUI, type the following command:
+
+          morpheus-gui
+
+        Or add Morpheus to your Applications folder with:
+
+          ln -sf #{prefix}/Morpheus.app /Applications
+
+        For more information about this branch visit: https://gitlab.com/morpheus.lab/morpheus/-/tree/release_2.2
+      EOS
+    end
+  end
+
+  test do
+    (testpath/"test.xml").write <<~EOF
+      <?xml version='1.0' encoding='UTF-8'?>
+      <MorpheusModel version="4">
+          <Description>
+              <Details></Details>
+              <Title></Title>
+          </Description>
+          <Space>
+              <Lattice class="linear">
+                  <Neighborhood>
+                      <Order>1</Order>
+                  </Neighborhood>
+                  <Size value="100,  0.0,  0.0" symbol="size"/>
+              </Lattice>
+              <SpaceSymbol symbol="space"/>
+          </Space>
+          <Time>
+              <StartTime value="0"/>
+              <StopTime value="0"/>
+              <TimeSymbol symbol="time"/>
+          </Time>
+          <Analysis>
+              <ModelGraph include-tags="#untagged" format="dot" reduced="false"/>
+          </Analysis>
+      </MorpheusModel>
+    EOF
+
+    assert_match "Simulation finished", shell_output("#{bin}/morpheus --file test.xml")
+  end
+end
