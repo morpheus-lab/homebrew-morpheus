903468dda4823aa16b5f5e7d8a12d8ca85974645
Diego Jahn <diego.jahn@tu-dresden.de>
2022-09-27 15:48:11 +0200

    morpheus 2.3.0
    * Remove comment
    * Manually add version
    
    Closes #7.
    
    Signed-off-by: Diego <dieg0@mailbox.org>

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -2,21 +2,15 @@ class Morpheus < Formula
   desc "Modelling environment for multi-cellular systems biology"
   homepage "https://morpheus.gitlab.io/"
   url "https://gitlab.com/morpheus.lab/morpheus/-/archive/v2.3.0_1/morpheus-v2.3.0_1.tar.gz"
+  version "2.3.0"
   sha256 "af978a1918af10d47c5af60924ad5d3d5bad24d14406eb6f7baf05a758b3eb60"
   license "BSD-3-Clause"
+  revision 1
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+(?:_?\d+)?)$/i)
   end
-
-  #  bottle do
-  #    root_url "https://github.com/morpheus-lab/homebrew-morpheus/releases/download/morpheus-2.2.6"
-  #    sha256 cellar: :any,                 arm64_monterey:               "f895c1c4757d1af1a99a795a80313d548cac928cf74f6c637101f57286f27110"
-  #    sha256 cellar: :any,                 monterey:                     "6e9961d869546b16d93976f6aff99393477d7cd783a10df716f872d7d96209dd"
-  #    sha256 cellar: :any,                 catalina:                     "d3f22b183cbb91227991e0e94440b8af0eeb05f0a56e90c2092dbd7bc2a7f4a5"
-  #    sha256 cellar: :any_skip_relocation, x86_64_linux:                 "3264fd1d6fc496ff6b76cbf51c46ccefd0eed5b76d75df6ca6b510258c23de88"
-  #  end
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
