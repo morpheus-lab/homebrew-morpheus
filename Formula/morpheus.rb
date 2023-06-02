85317e91fe4210fae37e2788db354c1deeddf1ba
Jörn Starruß <joern@starruss.de>
2023-06-02 16:53:04 +0200

    Serve brew style pickiness

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -19,12 +19,12 @@ class Morpheus < Formula
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
-  depends_on "ffmpeg" => :recommended # Runtime dependencies
   depends_on "gnuplot"
   depends_on "graphviz"
   depends_on "libomp"
   depends_on "libtiff"
   depends_on "qt@5"
+  depends_on "ffmpeg" => :recommended # Runtime dependencies
 
   uses_from_macos "bzip2"
   uses_from_macos "libxml2"
