c22d32bba1ee991fd984a9439689ebfad35dc2cf
Diego Jahn <jahndiego@mailbox.org>
2023-06-02 21:00:31 +0200

    morpheus: add 2.3.4 bottle (catalina)

--- Formula/morpheus.rb
+++ Formula/morpheus.rb
@@ -14,6 +14,7 @@ class Morpheus < Formula
     root_url "https://github.com/morpheus-lab/homebrew-morpheus/releases/download/morpheus-2.3.4"
     sha256 cellar: :any, arm64_ventura: "2d55e46043a264e2a55f73b4189e5c46d880437fe851ef3b10b2a7e4bdf3d0e8"
     sha256 cellar: :any, monterey:      "e6d87a28b2f7a1fff03aab80043cd3e1dc03b63fc728976f1496625707748e04"
+    sha256 cellar: :any, catalina:      "e1c1760a2672dbf85862af45e75255fa7002398fafd1546b3ca3f9eff9d43520"
   end
 
   depends_on "boost" => :build
